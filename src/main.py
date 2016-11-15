e20d3994ab5ad9f2530755a410b642b7339b9c17
Igor Ferreira <ipferreira@inf.ufrgs.br>
2016-11-15 13:07:25 -0200

    Usando Vader para análise de sentimentos

--- src/main.py
+++ src/main.py
@@ -1,9 +1,14 @@
 # coding=utf-8
 import nltk
+from nltk import word_tokenize
 import random
 import csv
 from parser import CsvReader
 from nltk.corpus import stopwords
+from nltk.collocations import BigramCollocationFinder
+from nltk.metrics import BigramAssocMeasures
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import itertools
 
 class Classifier:
     def __init__(self, target, train_set, test_set, classifier):
@@ -18,11 +23,25 @@ class Classifier:
 if __name__ == '__main__':
     targets = ["Atheism", "Hillary Clinton", "Donald Trump", "Legalization of Abortion",
                "Climate Change is a Real Concern", "Feminist Movement"]
+    option = 3
+    # verificar se elemento pertence ao conjunto é mais rápido do que com listas (O(1) vs O(n))
     stop_words = set(stopwords.words('english'))
 
-    # Bag of Words
-    def features(words):
-        return dict([(word, True) for word in words])
+    sia = SentimentIntensityAnalyzer()
+    def features(words, orig):
+        if option == 1: # Bag of Words
+            return dict([(word, True) for word in words if word not in stop_words])
+        if option == 2: # Bigrams
+            bigram_finder = BigramCollocationFinder.from_words(words)
+            bigrams = bigram_finder.nbest(BigramAssocMeasures.chi_sq, 10)
+            return dict([(ngram, True) for ngram in itertools.chain(words, bigrams)])
+        if option == 3:
+            f = {}
+            #f = dict([(word, True) for word in words if word not in stop_words])
+            ss = sia.polarity_scores(orig)
+            f["sentiment"] = sorted(ss, key=ss.get, reverse=True)[0]
+            return f
+
 
     parser = CsvReader("data/StanceDataset/test.csv")
     tweets = parser.parse()
@@ -30,10 +49,11 @@ if __name__ == '__main__':
 
     for (tweets, target) in tweets_by_target:  # removendo stopwords
         for tweet in tweets:
-            tweet.tweet = nltk.word_tokenize(tweet.tweet)
+            tweet.tweet = word_tokenize(tweet.tweet)
 
-        featuresets = [(features(n), tag) for (n, tag) in [(tweet.tweet, tweet.stance) for tweet in tweets]]
+        featuresets = [(features(n, o), tag) for (n, tag, o) in [(tweet.tweet, tweet.stance, tweet.original_tweet) for tweet in tweets]]
         size = int(len(featuresets) * 0.25)
         train_set, test_set = featuresets[size:], featuresets[:size]
         classifier = nltk.NaiveBayesClassifier.train(train_set)
+        #classifier.show_most_informative_features()
         print 'accuracy: ', target, nltk.classify.accuracy(classifier, test_set)
